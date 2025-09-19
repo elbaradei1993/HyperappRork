f21d555ec7a23893ba69ef4961a4b5f297e4735d
elbaradei1993 <fatehielbaradei2030@gmail.com>
2025-09-19 13:06:42 +0300

    Create expo-env.d.ts

---
+++ expo-env.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="expo/types" />
+
+// Ensure compatibility with Expo SDK
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.svg';
+declare module '*.gif';
